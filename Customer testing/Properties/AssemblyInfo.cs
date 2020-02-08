840129154422c386caa0f31aebd6b5ab0aab250d
Kabir Bahago <P2510339@LEC-ADMIN.dmu.ac.uk>
2020-02-08 14:04:08 +0000

    Add project files.

---
+++ Customer testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Customer testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Customer testing")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b33ff88-1975-4aa4-9227-8a4f58dc9dfd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomerClasses/ClsDataConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ClsDataConnection
+    {
+        //connection object used to connect to the database
+        sqlConnection connectionToDB = new sqlConnection();
+        //data adapter used to transfer data to and from the database
+        sqlAdapter dataChannel = new sqlAdapter();
+
+    }
+    set 
+    { 
+     //set the query results
+     dataTable = Value;
+    }
+}
+}
+}// this closes the namspace!
+
+
+
